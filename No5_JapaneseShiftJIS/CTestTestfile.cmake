8d357442dedc7eac4b54b73d5b8c90834239da0f
Katayama Hirofumi MZ <katayama.hirofumi.mz@gmail.com>
2017-11-06 12:21:30 +0900

    improve

--- No5_JapaneseShiftJIS/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/katahiromz/Desktop/BoostWaveExample/No5_JapaneseShiftJIS
-# Build directory: C:/Users/katahiromz/Desktop/BoostWaveExample/No5_JapaneseShiftJIS
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(wave_utf8_test "C:/Users/katahiromz/Desktop/BoostWaveExample/build/wave_utf8_test.exe")
